4a662254833fe36d9c8727459e4196126188140b
Olstra <seriously251@gmail.com>
2021-03-19 17:53:22 +0000

    contract_begin done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Import dependencies
 const fs = require("fs");
 const PDFParser = require("pdf2json");
+const unixTimestamp = require("unix-timestamp");
 
 // Get all the filenames from the patients folder
 const files = fs.readdirSync("pdf");
@@ -29,10 +30,35 @@ let patients = [];
 
                 // The raw PDF data in text form
                 const raw = pdfParser.getRawTextContent().replace(/\r\n/g, " ");
-
+                
                 // Return the parsed data
                 resolve({
-                    nameeee: /Allgemeine Vertragsangaben\s+[a-zA-z]+\s+([\d]{2}\.[\d]{2}.[\d]{4})/i.exec(raw),
+                    yearly_cancellation: "0",
+                    validity: "1362009600",
+                    contract_begin: /Allgemeine Vertragsangaben[a-zA-z\s]*\s*([\d]{2}\.[\d]{2}.[\d]{4})/.exec(raw)[1].trim(),
+                    contract_termination: "1451520000",
+                    maximum_insured_salary: {
+                        industrial_accident: {
+                            men: "2774000",
+                            women: "12234900"
+                        },
+                        non_industrial_accident: {
+                            men: "2758300",
+                            women: "12156300"
+                        }
+                    },
+                    premium_rate: {
+                        industrial_accident: {
+                            "overall": "3.32"
+                        },
+                        non_industrial_accident: {
+                            overall: "10.86"
+                        }
+                    },
+                    yearly_premium: "211802",
+                    premiums: {
+                        discount: "0"
+                    }
                 });
 
             });
@@ -44,7 +70,11 @@ let patients = [];
 
     }));
 
+    //console.log(patients[0]["contract_begin"]); // access var in array
+    //let temp = "25.10.2019".split('.').reverse().join('-'); // convert date format "dd.mm.yyyy -> yyyy-mm-dd"
+    patients[0]["contract_begin"] = new Date(patients[0]["contract_begin"].split('.').reverse().join('-')).getTime() / 1000;
+    
     // Save the extracted information to a json file
-    fs.writeFileSync("patients.json", JSON.stringify(patients));
+    fs.writeFileSync("offerteDaten.json", JSON.stringify(patients));
 
 })();
